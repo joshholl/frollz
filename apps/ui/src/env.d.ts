cc03a4b1e9cb1d4005a91ead3d59eaa83d284227
Josh Hollandsworth <142175+joshholl@users.noreply.github.com>
2026-04-25 15:29:19 -0400

    chore(ignore): removing quasar files and other items that should be ignored

--- apps/ui/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="@quasar/app-vite" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-
-  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
-  export default component;
-}
